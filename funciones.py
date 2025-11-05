2af32e4ae330f4e8c11f9246e190035a5cb0a7a0
cynthia04-code <cynthia04@ciencias.unam.mx>
2025-11-05 08:55:30 -0600

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,20 @@
+def usar_funcion(mi_funcion, numero):
+    return mi_funcion(numero)
+
+def duplicar(x):
+    return x * 2
+
+def cuadrado (x):
+     return x * x 
+
+if __name__ == "__main__":
+    print("=== MI PROGRAMA FUNCIONAL ===")
+
+    resultado1 = usar_funcion(duplicar; 5)
+    resultado2 = usar_funcon(lambda 4)
+    resultado3 = usar_funcion(lambda x: x +10, 5)
+
+    print(f"Duplicar 5: {resultado1}")
+    print(f"Cuadrado de 4: {resultado2}")
+    print(f"5 + 10: { resultado3}")
+  
